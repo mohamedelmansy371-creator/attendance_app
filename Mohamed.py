285f6973ef4222b074fe5449e9ffcef72bdfd849
mohamedelmansy371-creator <mohamedelmansy371@gmail.com>
2026-09-26 04:23:29 +0300

    Update Mohamed.py

--- Mohamed.py
+++ Mohamed.py
@@ -18,8 +18,8 @@ CLASS_LAT = 30.718881
 CLASS_LON = 31.244633
 ALLOWED_RADIUS_METERS = 100
 
-# رابط الـ Web App الخاص بملف Google Sheets الخاص بك
-GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbxvYZXJaC6rgpchmf2jN8TgzzrbukHmc-BiTGVtGBa2XzcJvwVo5oJGcN5LiEgX9j3v2A/exec"
+# رابط الـ Web App الجديد الخاص بك
+GOOGLE_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbzv9WIfVeNH_PxMyqhsrLJKj0svRQ76vmBJdzYUpNl745-n0LQ__WyXVHWTHy4jMQfo/exec"
 
 form_html = (
     """
